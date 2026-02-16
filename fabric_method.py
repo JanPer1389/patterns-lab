64feb805794cf974711c6d9c750179dd054d0872
JanPer1389 <josefvorobyev133@gmail.com>
2026-02-16 14:38:12 +0300

    fabric_method

---
+++ fabric_method.py
@@ -0,0 +1,26 @@
+from enum import Enum
+from math import *
+
+class CoordinateSystem(Enum):
+    Cartesian = 1
+    Polar = 2
+    
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def __str__(self):
+        return f'x = {self.x}, y= {self.y}'
+    
+    @staticmethod
+    def new_cartesian_method(x,y):
+        return Point(x,y)
+    @staticmethod
+    def new_polar_method(x,y):
+        return Point(x * sin(y), x*cos(y))
+
+if __name__=='__main__':
+    p = Point(2,5)
+    p2= Point.new_polar_method(2,5)
+    print(p, p2)
